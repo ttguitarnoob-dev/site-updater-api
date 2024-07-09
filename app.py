11fa98da8c678726f3e282929103e7c1a09d83dc
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-07-09 09:42:28 -0500

    flask boilerplate done

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 from flask import Flask
+
+# Create a Flask application
+app = Flask(__name__)
+
+# Define a route and a view function
+@app.route('/')
+def hello():
+    return 'Hello, World!'
+
+# Run the application
+if __name__ == '__main__':
+    app.run(debug=True)
