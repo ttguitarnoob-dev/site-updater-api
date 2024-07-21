becb235e55058152ab806640f38cc9856a486c6f
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-07-21 17:32:35 -0500

    maybe work

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def assupdate():
 
 # Run the application
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=False allow_unsafe_werkzeug=True)
